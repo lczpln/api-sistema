daee920b14f08287ac1f24c4ddb03d66a0c7a0ba
Lucas Pellison <lucaspellison@gmail.com>
2018-12-02 13:57:41 -0200

    new update (delete)

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ route.use(bodyParser.urlencoded({ extended: false }));
 route.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 })
 
