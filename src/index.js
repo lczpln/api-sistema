54118521caadc92eadbcfc7d7820947f7f77da5c
Lucas Pellison <lucaspellison@gmail.com>
2018-12-04 20:22:20 -0200

    COOOORS

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ const routes = require('./routes');
 
 // Express
 const app = express();
-app.use(cors({credentials: true}));
 
 // Socket.io
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-io.set('origins', '*:*');
 
 // Conecta ao banco de dados
 mongoose.connect('mongodb://site:site123@ds041167.mlab.com:41167/site-backend', { useNewUrlParser: true });
