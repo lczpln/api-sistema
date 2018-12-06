8f264dc75a276abcc5b2f9df6bfd42b2ba65f905
lczpln <37776923+lczpln@users.noreply.github.com>
2018-12-06 10:28:39 -0200

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const http = require('http');
 const cors = require('cors');
-
-// Express
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 
 // Cors
 const configCors = {
@@ -14,11 +14,6 @@ const configCors = {
 }
 
 app.use(cors(configCors));
-
-
-// Socket.io
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
 
 // Conecta ao banco de dados
 mongoose.connect('mongodb://site:site123@ds041167.mlab.com:41167/site-backend', { useNewUrlParser: true });
@@ -33,11 +28,11 @@ app.use((req, res, next) => {
 const routes = require('./routes');
 app.use(routes);
 
-//Porta randomica
+//Normalizar porta
 var porta = process.env.PORT || 3000;
 
 //Ouve a porta 
-app.listen(porta, () => {
+server.listen(porta, () => {
     console.log("Server listening on port " + porta + " (:");
 })
 
